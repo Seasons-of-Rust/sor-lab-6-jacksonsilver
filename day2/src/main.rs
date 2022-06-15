27d68bf861d7ec2dfbf79ee143f511d0cdb77bcd
jacksonsilver <jacksonsilver@cmail.carleton.ca>
2022-06-14 20:18:50 -0400

    Update main.rs

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -17,25 +17,22 @@ fn main() {
         })
         .collect();
 
-    let mut index: i32;
-    let mut numbers2: Vec<i32>;
-
     for noun in 0..=99 {
         numbers[1] = noun;
         for verb in 0..=99 {
             numbers[2] = verb;
-            numbers2 = numbers.clone();
+            let mut numbers2 = numbers.clone();
 
             for i in (0..numbers2.len()).step_by(4) {
                 match numbers2[i as usize] {
                     1 => {
-                        index = numbers2[(i + 3) as usize];
+                        let index = numbers2[(i + 3) as usize];
                         numbers2[index as usize] = numbers2[numbers2[(i + 1) as usize] as usize]
                             + numbers2[numbers2[(i + 2) as usize] as usize];
                         numbers2[i as usize] += 4;
                     }
                     2 => {
-                        index = numbers2[(i + 3) as usize];
+                        let index = numbers2[(i + 3) as usize];
                         numbers2[index as usize] = numbers2[numbers2[(i + 1) as usize] as usize]
                             * numbers2[numbers2[(i + 2) as usize] as usize];
                         numbers2[i as usize] += 4;
